8d99d9ebe088cfa985173f1a9a22d5261b904d88
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2021-02-16 21:15:47 +0700

    Wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.guest title="Login">
     Login
+    <div class="mb-4">
+        <x-label for="email">Email</x-label>
+        <x-input type="text" name="email" id="email"/>
+    </div>
 </x-layouts>
