6a2803a1b276e27952b433995772a1b76efbd725
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2021-02-17 10:26:33 +0700

    Wip

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -38,6 +38,6 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        Storage::copy(__DIR__ . '/../../fence/package.json', __DIR__);
+        Storage::copy(__DIR__ . '/../../fence/package.json', base_path('package.json'));
     }
 }
