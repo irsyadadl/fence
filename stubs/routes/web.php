a47897cb5f16b3858fbd81fd3a1f7358e77def40
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2021-02-19 13:59:04 +0700

    wip-ah

--- stubs/routes/web.php
+++ stubs/routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 Route::view('/', 'welcome');
 Route::get('/home', HomeController::class);
+
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
 });
